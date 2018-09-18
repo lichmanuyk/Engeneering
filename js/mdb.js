6d6f30f41a6e75c704893907f64f035d7a5f6d1c
Pavel Lichmanuyk <pavel.lichmanuyk@gmail.com>
2018-09-18 15:59:26 +0300

    Create navbar

--- js/mdb.js
+++ js/mdb.js
@@ -15441,7 +15441,6 @@ Waves.attach('.btn-flat', ['waves-effect']);
 Waves.attach('.chip', ['waves-effect']);
 Waves.attach('.view a .mask', ['waves-light']);
 Waves.attach('.waves-light', ['waves-light']);
-Waves.attach('.navbar-nav a:not(.navbar-brand), .nav-icons li a, .nav-tabs .nav-item:not(.dropdown)', ['waves-light']);
 Waves.attach('.pager li a', ['waves-light']);
 Waves.attach('.pagination .page-item .page-link', ['waves-effect']);
 Waves.init();
